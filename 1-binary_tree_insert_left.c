07e52e6f0a0653e6fa7c16ff94db080c0a4acf62
Alex Jarabi <jarabialex@gmail.com>
2023-08-01 01:04:58 +0300

    Adding a function that inserts a node as the left-child of another node.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -35,5 +35,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	/* Set parent left to new node */
 	parent->left = new_node;
 
-	return(new_node);
+	return (new_node);
 }
