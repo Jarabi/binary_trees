0d9f203749786b02d9fb543f8f2cf2a9f008be88
Alex Jarabi <jarabialex@gmail.com>
2023-08-03 23:04:26 +0300

    Correction for tree balance factor.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
-
-int get_diff(const binary_tree_t *tree);
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
@@ -10,39 +9,16 @@ int get_diff(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int difference;
-
-	if (!tree)
-		return (0);
-
-	difference = get_diff(tree);
-	return (difference - 1);
-}
-
-/**
- * get_diff - get difference between left and right sub-trees
- * @tree: pointer to the root node of the tree
- *
- * Return: height difference of tree or 0 if tree is NULL
- */
-int get_diff(const binary_tree_t *tree)
-{
-	int left_height, right_height, diff;
-
-	left_height = 0;
-	right_height = 0;
-	diff = 0;
+	int left_height, right_height;
 
 	if (!tree)
 		return (0);
 
 	/* Get height of left sub_tree */
-	left_height = get_diff(tree->left);
+	left_height = get_max_height(tree->left);
 
 	/* Get height of right sub-tree */
-	right_height = get_diff(tree->right);
+	right_height = get_max_height(tree->right);
 
-	diff = 1 + (left_height - right_height);
-
-	return (diff);
+	return (left_height - right_height);
 }
